93198516219ce9abdaa6fa7c8f21720968a354bc
unknown <rogeliomdn@gmail.com>
2023-11-24 22:42:04 +0800

    Exercise 3.4

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ app.get(`${apiUrl}/:id`, (request, response) => {
     }
 });
 
+app.delete(`${apiUrl}/:id`, (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
