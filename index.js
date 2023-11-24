fbd48d6cd1d639906c4867920340529e9f277436
unknown <rogeliomdn@gmail.com>
2023-11-24 22:39:11 +0800

    Exercise 3.3

--- index.js
+++ index.js
@@ -36,15 +36,26 @@ app.get('/info', (request, response) => {
     );
 });
 
-const url = "/api/persons";
+const apiUrl = "/api/persons";
 
-app.get(url, (request, response) => {
+app.get(apiUrl, (request, response) => {
     response.json(persons);
     console.log('request :>> ', request);
     console.log('response :>> ', response);
-})
+});
+
+app.get(`${apiUrl}/:id`, (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
 
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
