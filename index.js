68ff1b4c6f39ebc4338e0cd6a723b36c79eecad9
unknown <rogeliomdn@gmail.com>
2023-11-25 21:20:07 +0800

    Exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json());
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            (tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : '')
+        ].join(' ')
+    }));
 
 let persons = [
     {
@@ -90,7 +102,6 @@ app.post(apiUrl, (request, response) => {
     }
 
     persons = persons.concat(person);
-    console.log('person saved :>> ', person);
     response.json(person);
 });
 
