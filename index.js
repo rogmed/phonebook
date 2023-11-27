855bcca54063d2c756764fe1ae99600110552332
unknown <rogeliomdn@gmail.com>
2023-11-27 21:46:23 +0800

    Fly configuration

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const app = require('cors')
 
+app.use(cors());
 app.use(express.json());
 app.use(
     morgan(function (tokens, req, res) {
