8bbe00744072411bd6684aeb3ff71469d975f424
unknown <rogeliomdn@gmail.com>
2023-11-27 22:01:19 +0800

    Revert "Fly configuration"
    This reverts commit 855bcca54063d2c756764fe1ae99600110552332.

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const app = require('cors')
 
-app.use(cors());
 app.use(express.json());
 app.use(
     morgan(function (tokens, req, res) {
